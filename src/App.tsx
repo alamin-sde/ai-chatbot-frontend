2d0ab54a4148df770bf733d0c6fe162df50d85ed
alamin-sde <ai.alamin.sde@gmail.com>
2025-09-25 12:52:25 +0530

    protectRoute page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,34 @@
 
-import Navbar from './components/layout/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Test from './pages/Test'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import { Toaster } from 'react-hot-toast'
-import { useLoading } from './contexts/LoadingContext'
-import LoadingSpinner from './components/ui/LoadingSpinner'
-import Chat from './pages/Chat'
-import Sidebar from './components/layout/Sidebar'
 import { useAuth } from './contexts/AuthContext'
 import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './pages/ProtectedRoute'
+import ChatPage from './pages/ChatPage'
 
 function App() {
-  const { loading } = useLoading();
-  const { user, currentView } = useAuth();
-  console.log("user in app==>", currentView)
-  if (loading) {
-    return (
-      <div className='flex justify-center items-center min-h-screen'>
-        <LoadingSpinner size='sm' className='' />
-      </div>
-    )
-  }
-  if (currentView === 'dashboard') {
-    return (
-      <Dashboard />
-    )
-
-  } else if (currentView === 'login') {
-    return (
-      <Login />
-    )
-
-  } else if (currentView === "register") {
-    return (
-      <Register />
-    )
-  }
+  const { user } = useAuth();
   return (
-    <div className='bg-grey-50'>
-      <Navbar />
-      <Sidebar />
-      <Chat/>
-      {/* <Routes>
+    <div>
+      {/* {user && <Navbar />}
+      {user && <Sidebar />} */}
+      <Routes>
         <Route path='/test' Component={Test} />
+        <Route path='/' Component={Dashboard} />
         <Route path='/register' Component={Register} />
         <Route path='/login' Component={Login} />
-        <Route path='/chat' Component={Chat} />
-      </Routes> */}
+        <Route
+          path='/chat'
+          element={
+            <ProtectedRoute>
+              <ChatPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
       <Toaster
         position='top-right'
         toastOptions={{
