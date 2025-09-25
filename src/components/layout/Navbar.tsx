4aaf9b437eb8ebbea695447cada5c209698a3c0d
alamin-sde <ai.alamin.sde@gmail.com>
2025-09-25 12:51:27 +0530

    in navbar user profile and darkmode added

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,14 @@
-import { Menu, MessageCircle, Moon, Sun, X } from "lucide-react";
+import { Menu, Moon, Sun, X } from "lucide-react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { useTheme } from "../../contexts/ThemeContext";
 import { useSidebar } from "../../contexts/SidebarContext";
+import aichatbot from "../../assets/ai-chatbot.jpg";
+import { useAuth } from "../../contexts/AuthContext";
+
 const Navbar = () => {
     const { theme, toggleTheme } = useTheme()
     const { toggleSidebar } = useSidebar();
+    const {user}=useAuth();  
     const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
     return (
         <nav className="text-white border-b shadow-sm border-gray-200 dark:bg-gray-800 dark:border-gray-700">
@@ -20,36 +23,25 @@ const Navbar = () => {
                             <Menu size={20} />
                         </button>
                     </div>
-
-                    {/* <div className="flex justify-center items-center space-x-4 px-4">
-                        <div className="flex-1">
-                            <button
-                                className="text-gray-600"
-                            >
-                                <Menu size={20} />
-                            </button>
-                        </div>
-                        <Link to="/" className=" flex justify-center items-center space-x-2">
-                            <div className="w-8 h-8 bg-gradient-to-br from-primary-500 to-secondary-500 rounded-lg flex justify-center items-center">
-                                <MessageCircle className="w-5 h-5  text-white" />
-                            </div>
-                            <span className="text-xl font-bold text-gray-900 dark:text-white">AI Chat</span>
-                        </Link>
-                    </div> */}
-
-                    {/* Desktop Navigation */}
                     <div className="flex justify-between">
-                        {/* <Link to="/login" className=" text-gray-600 font-medium px-3 py-2  dark:text-gray-300">
-                            Login
-                        </Link> */}
                         <button className="rounded-lg text-gray-500 p-2"
                             onClick={() => toggleTheme()}
                         >
                             {theme === "dark" ? <Sun size={20} /> : <Moon size={20} />}
                         </button>
-                        <Link to="/register" className="btn-primary">
-                            Login
-                        </Link>
+                        <div>
+
+                            <button
+                            className="flex justify-center items-center space-x-2 hover:bg-gray-100 p-2 rounded-lg"
+                            >
+                                <img
+                                    src={aichatbot}
+                                    className="w-8 h-8 rounded-full"
+                                />
+                                <p className="text-gray-900">{user.username}</p>
+
+                            </button>
+                        </div>
 
 
                     </div>
